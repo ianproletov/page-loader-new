6883e6c030aa5868e0c8341ea7da8e995a4cb626
ianproletov <yan.proletov@gmail.com>
2020-12-14 21:41:21 +0300

    add image loading

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -18,20 +18,10 @@ beforeEach(async () => {
   tmpDir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
 
-test('download test', async () => {
-  const expectedData = await readFile('page-actual.html');
-  nock('https://proletov.com')
-    .get('/page')
-    .reply(200, expectedData);
-  await loadPage('https://proletov.com/page', tmpDir);
-  const actualData = await fs.readFile(path.join(tmpDir, 'proletov-com-page.html'), 'utf-8');
-  expect(actualData).toBe(expectedData);
-});
-
 test('transform image link', async () => {
   const expectedData = await readFile('page-expected.html');
   const beforeData = await readFile('page-actual.html');
-  const imageData = await readFile('image.png');
+  const imageData = await fs.readFile(getFixturesPath('image.png'));
   nock('https://proletov.com')
     .get('/page')
     .reply(200, beforeData)
@@ -40,7 +30,7 @@ test('transform image link', async () => {
   await loadPage('https://proletov.com/page', tmpDir);
   const actualData = await fs.readFile(path.join(tmpDir, 'proletov-com-page.html'), 'utf-8');
   expect(actualData).toBe(expectedData);
-  const receivedImageDataPath = path.join(tmpDir, '_files', 'proletov-com-assets-image.png');
-  const receivedImageData = fs.readFile(receivedImageDataPath);
-  expect(receivedImageData).toBe(expectedData);
+  const receivedImageDataPath = path.join(tmpDir, 'proletov-com-page_files', 'assets-image.png');
+  const receivedImageData = await fs.readFile(receivedImageDataPath);
+  expect(receivedImageData.toString()).toBe(imageData.toString());
 });
--- src/index.js
+++ src/index.js
@@ -2,18 +2,55 @@ import _ from 'lodash';
 import axios from 'axios';
 import { promises as fs } from 'fs';
 import path from 'path';
+import cheerio from 'cheerio';
+import url from 'url';
 
-const getName = (pageAddress) => {
-  const { host, pathname } = new URL(pageAddress);
-  const changed = `${host}${_.trimEnd(pathname, '/')}`.replace(/\W/g, '-');
-  return `${changed}.html`;
+const types = {
+  main: (pageAddress) => {
+    const { host, pathname } = url.parse(pageAddress);
+    return `${host}${_.trimEnd(pathname, '/')}`.replace(/\W/g, '-');
+  },
+  file: (pageAddress) => `${types.main(pageAddress)}.html`,
+  link: (pageAddress) => {
+    const { dir, name, ext } = path.parse(pageAddress);
+    return `${_.trimStart(path.join(dir, name), '/')}`.replace(/\W/g, '-').concat(`${ext}`);
+  },
+  dir: (pageAddress) => `${types.main(pageAddress)}_files`,
 };
 
-const loadPage = (pageAddress, outputPath) => axios.get(pageAddress)
-  .then(({ data }) => {
-    const filename = getName(pageAddress);
-    const filepath = path.join(outputPath, filename);
-    return fs.writeFile(filepath, data.toString());
-  });
+const getName = (pageAddress, type) => types[type](pageAddress);
+
+const loadPage = (pageAddress, outputPath) => {
+  const linkDir = getName(pageAddress, 'dir');
+  const loadedLinks = [];
+  return axios.get(pageAddress)
+    .then(({ data }) => {
+      const filename = getName(pageAddress, 'file');
+      const filepath = path.join(outputPath, filename);
+      const $ = cheerio.load(data);
+      $('base').remove();
+      $('img').each((index, currentTag) => {
+        const link = $('img').attr('src');
+        if (link && !url.parse(link).host) {
+          const linkName = getName(link, 'link');
+          const linkPath = path.join(linkDir, linkName);
+          loadedLinks.push(link);
+          $(currentTag).attr('src', linkPath);
+        }
+      });
+      fs.mkdir(path.join(outputPath, linkDir));
+      return fs.writeFile(filepath, $.html());
+    })
+    .then(() => {
+      const promises = loadedLinks.map((link) => axios.get(url.resolve(pageAddress, link), { responseType: 'arraybuffer' })
+        .then(({ data }) => {
+          const linkName = getName(link, 'link');
+          const linkPath = path.join(linkDir, linkName);
+          const absoluteLinkPath = path.join(outputPath, linkPath);
+          return fs.writeFile(absoluteLinkPath, data);
+        }));
+      return Promise.all(promises);
+    });
+};
 
 export default loadPage;
